34a663b7e0567abe846b9d2e375c18dc0c0f26b7
Akko <2322264747@qq.com>
2020-04-12 23:16:45 +0800

    Create StudentMarkSheetData.js

---
+++ E2/js/StudentMarkSheetData.js
@@ -0,0 +1,57 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-04-12 23:06:10
+ * -> LastEditTime : 2020-04-12 23:06:24
+ * -> LastEditors : your name
+ * -> Description : Sheet Data
+ * -> FilePath : \Web_Exercises\E2\js\StudentMarkSheetData.js
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
+document.writeln("                <tr>");
+document.writeln("                    <td style=\'width: 15%;\'>Name</td>");
+document.writeln("                    <td style=\'width: 10%;\'>Maths</td>");
+document.writeln("                    <td style=\'width: 10%;\'>Science</td>");
+document.writeln("                    <td style=\'width: 10%;\'>English</td>");
+document.writeln("                    <td style=\'width: 10%;\'>Physics</td>");
+document.writeln("                    <td style=\'width: 30%;\'>General Knowledge</td>");
+document.writeln("                </tr>");
+document.writeln("                <tr>");
+document.writeln("                    <td>David</td>");
+document.writeln("                    <td>85</td>");
+document.writeln("                    <td>87</td>");
+document.writeln("                    <td>88</td>");
+document.writeln("                    <td>92</td>");
+document.writeln("                    <td>88</td>");
+document.writeln("                </tr>");
+document.writeln("                <tr>");
+document.writeln("                    <td>Richard</td>");
+document.writeln("                    <td>91</td>");
+document.writeln("                    <td>81</td>");
+document.writeln("                    <td>78</td>");
+document.writeln("                    <td>71</td>");
+document.writeln("                    <td>74</td>");
+document.writeln("                </tr>");
+document.writeln("                <tr>");
+document.writeln("                    <td>John</td>");
+document.writeln("                    <td>81</td>");
+document.writeln("                    <td>86</td>");
+document.writeln("                    <td>88</td>");
+document.writeln("                    <td>84</td>");
+document.writeln("                    <td>92</td>");
+document.writeln("                </tr>");
+document.writeln("                <tr>");
+document.writeln("                    <td>Tony</td>");
+document.writeln("                    <td>84</td>");
+document.writeln("                    <td>86</td>");
+document.writeln("                    <td>87</td>");
+document.writeln("                    <td>82</td>");
+document.writeln("                    <td>81</td>");
+document.writeln("                </tr>");
+document.writeln("                <tr>");
+document.writeln("                    <td>Scott</td>");
+document.writeln("                    <td>71</td>");
+document.writeln("                    <td>79</td>");
+document.writeln("                    <td>82</td>");
+document.writeln("                    <td>88</td>");
+document.writeln("                    <td>89</td>");
+document.writeln("                </tr>");
